7426f0848abb7d0e116ddb1d9267db7980baf272
TheAsda <omega-faworit@yandex.ru>
2020-01-04 22:02:17 +0300

    feat: add displaying paths

--- src/components/GetPath.tsx
+++ src/components/GetPath.tsx
@@ -1,13 +1,25 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { Button } from "@progress/kendo-react-buttons";
 import "../styles/GetPath.scss";
 import { PointsContext } from "../App";
 import { IPickPoint } from "../interfaces/PickPoint";
+import { getPath } from "../API";
+import { IPath } from "../interfaces/Path";
 
 const GetPath = () => {
   const points: IPickPoint[] = useContext(PointsContext);
+  const [path, setPath] = useState<{ from: string; to: string }>({
+    from: "",
+    to: ""
+  });
+  const [data, setData] = useState<IPath>();
 
+  const get = () => {
+    getPath(path).then(res => {
+      setData(res);
+    });
+  };
   return (
     <div className="path__page">
       <h1>Get path</h1>
@@ -18,6 +30,9 @@ const GetPath = () => {
           data={points}
           dataItemKey="id"
           textField="address"
+          onChange={e =>
+            setPath(Object.assign(path, { from: e.target.value.id }))
+          }
         />
         <DropDownList
           label="Finish point"
@@ -25,9 +40,25 @@ const GetPath = () => {
           data={points}
           dataItemKey="id"
           textField="address"
+          onChange={e =>
+            setPath(Object.assign(path, { to: e.target.value.id }))
+          }
         />
-        <Button>Get path</Button>
+        <Button onClick={get}>Get path</Button>
       </div>
+      {data && (
+        <div>
+          <h2>Total distance: {data.totalDistance.toFixed(0)} km</h2>
+          <h2>Total time: {data.totalTime.toFixed(0)} mins</h2>
+          <div className="addresses">
+            {data.path.map((address, i) => (
+              <h3 className="address" key={i}>
+                {`${i + 1}. ${address}`}
+              </h3>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
