a65df66cab490efdb274138b79e350905fb10cf8
TheAsda <omega-faworit@yandex.ru>
2020-01-04 21:25:45 +0300

    feat: finished closing orders

--- src/components/CloseOrder.tsx
+++ src/components/CloseOrder.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { closeOrder } from "../API";
 import { Button } from "@progress/kendo-react-buttons";
+import { OrdersContext } from "../App";
 
 const CloseOrder = () => {
-  const close = (id: string) => {
-    closeOrder(id);
+  const orders = useContext(OrdersContext)
+    .filter(item => item.status !== "closed" && item.paid === true)
+    .map(item => ({ id: item.id, title: item.package.title }));
+  const [orderId, setId] = useState<string>("");
+
+  const close = () => {
+    console.log(orderId);
+    closeOrder(orderId);
   };
+
   return (
     <div className="pay">
       <h1>Close order</h1>
-      <DropDownList label="Order" style={{ backgroundColor: "#ffeb3b" }} />
-      <Button>Close</Button>
+      <DropDownList
+        label="Order"
+        style={{ backgroundColor: "#ffeb3b" }}
+        data={orders}
+        textField="title"
+        dataItemKey="id"
+        onChange={e => setId(e.target.value.id)}
+      />
+      <Button onClick={close}>Close</Button>
     </div>
   );
 };
--- src/components/GetPaid.tsx
+++ src/components/GetPaid.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { DatePicker } from "@progress/kendo-react-dateinputs";
 import "../styles/GetPaid.scss";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { Button } from "@progress/kendo-react-buttons";
+import { DistrictsContext } from "../App";
 
 const GetPaid = () => {
+  const districts = useContext(DistrictsContext);
   return (
     <div className="get__paid">
       <h1>Get paid orders by district and date</h1>
       <div>
-        <DropDownList style={{ backgroundColor: "#ffeb3b" }} />
+        <DropDownList
+          style={{ backgroundColor: "#ffeb3b" }}
+          data={districts}
+          dataItemKey="name"
+          textField="name"
+        />
         <DatePicker />
         <Button>Get orders</Button>
       </div>
